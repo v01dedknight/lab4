1ea4fce89717e2aa859f751718684159188a5108
v01d <Kevin_Vearez@mail.ru>
2026-04-03 09:11:39 +0700

    The base class of a text file and serialization mechanisms

--- Program.cs
+++ Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace lab4 {
-  internal class Program {
-    static void Main(string[] args) {
+namespace FileManager.Models {
+  [Serializable]
+  public class TextFileData {
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime LastModified { get; set; }
+
+    public TextFileData() { }
+
+    public TextFileData(string title, string content) {
+      Title = title;
+      Content = content;
+      LastModified = DateTime.Now;
+    }
+
+    // XML Сериализация
+    public void SaveToXml(string filePath) {
+      XmlSerializer serializer = new XmlSerializer(typeof(TextFileData));
+      using (StreamWriter writer = new StreamWriter(filePath)) {
+        serializer.Serialize(writer, this);
+      }
+    }
+
+    public static TextFileData LoadFromXml(string filePath) {
+      XmlSerializer serializer = new XmlSerializer(typeof(TextFileData));
+      using (StreamReader reader = new StreamReader(filePath)) {
+        return (TextFileData)serializer.Deserialize(reader);
+      }
+    }
+
+    // Бинарная сериализация
+    public void SaveToBinary(string filePath) {
+#pragma warning disable SYSLIB0011
+      BinaryFormatter formatter = new BinaryFormatter();
+      using (FileStream fs = new FileStream(filePath, FileMode.Create)) {
+        formatter.Serialize(fs, this);
+      }
+    }
+
+    public static TextFileData LoadFromBinary(string filePath) {
+#pragma warning disable SYSLIB0011
+      BinaryFormatter formatter = new BinaryFormatter();
+      using (FileStream fs = new FileStream(filePath, FileMode.Open)) {
+        return (TextFileData)formatter.Deserialize(fs);
+      }
     }
   }
 }
