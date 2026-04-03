bb29f558efde7d28a013573eb39640745344d73a
v01d <Kevin_Vearez@mail.ru>
2026-04-03 10:31:26 +0700

    Menu edited

--- Program.cs
+++ Program.cs
@@ -4,8 +4,50 @@ using FileManager.Models;
 using FileManager.Services;
 using FileManager.Core;
 
+// Элементы меню
+string inputComand = "";
+string textForFile = "";
+string word = "";
+
+// Экземпляры для проверки
 TextFileData currentFile = new TextFileData("NewDocument", "");
 EditorHistory history = new EditorHistory();
+FileSearcher searcher = new FileSearcher();
+
+Console.WriteLine("Консольный редактор\n"+
+  "Команды: 'add [текст]', 'undo', 'save', 'search', 'exit'");
+
+// Меню
+while (true) {
+  Console.Write("\n> ");
+  inputComand = Console.ReadLine();
+
+  // StartsWith используется для проверки, начинается ли строка с определенного текста
+  if (inputComand.StartsWith("add ")) {
+    textForFile = inputComand.Substring(4);
+    AddText(textForFile);
+    Console.WriteLine($"Добавлено. Текущий текст: {currentFile.Content}");
+  }
+  else if (inputComand == "undo") {
+    Undo();
+    Console.WriteLine($"Откат выполнен. Текущий текст: {currentFile.Content}");
+  }
+  else if (inputComand == "save") {
+    currentFile.SaveToXml("document.xml");
+    Console.WriteLine("Файл сохранен в XML (document.xml)");
+  }
+  else if (inputComand == "search") {
+    Console.Write("Введите ключевое слово для поиска в текущей папке: ");
+    word = Console.ReadLine();
+    searcher.IndexDirectory(Directory.GetCurrentDirectory(), new[] { word });
+  }
+  else if (inputComand == "exit") {
+    break;
+  }
+  else {
+    Console.WriteLine("Неизвестная команда.");
+  }
+}
 
 // Логика добавления
 void AddText(string newText) {
